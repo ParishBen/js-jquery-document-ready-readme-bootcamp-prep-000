a6290c7535583d3f980ade66fc42ff153ac2603b
Ben Parish <parishben13@gmail.com>
2020-04-17 06:03:05 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     // code to be executed goes here
-   $("#text").append("this is so freaking cool.");
+   $("#text").append("This is so freaking cool!");
   
 });
 
